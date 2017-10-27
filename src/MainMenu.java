68bdfcbe14fee34acb272d2c62270d1f8633ac63
gutsuv <victorgutsu@gmail.com>
2017-10-27 11:36:56 -0400

    for my boy andres

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -14,13 +16,13 @@ public class MainMenu extends TestStage
 		HBox hbox1 = new HBox();
 		Label title = new Label("Text Based Adventure Game");
 
-		Button newGame = new Button("New Game");
-		Button loadgame = new Button("Load Game");
-		Button helpMenu = new Button("Help Menu");
-		Button exitGame = new Button("Exit Game");
+		Button newGameButton = new Button("New Game");
+		Button loadGameButton = new Button("Load Game");
+		Button helpMenuButton = new Button("Help Menu");
+		Button exitGameButton = new Button("Exit Game");
 		
 		vbox1.getChildren().addAll(hbox1, vbox2);
-		vbox2.getChildren().addAll(newGame, loadgame, helpMenu, exitGame);
+		vbox2.getChildren().addAll(newGameButton, loadGameButton, helpMenuButton, exitGameButton);
 		hbox1.getChildren().addAll(title);
 		
 		VBox.setMargin(hbox1, new Insets(30, 0, 60, 0));
@@ -28,5 +30,32 @@ public class MainMenu extends TestStage
 		title.setFont(new Font("Times New Roman", 30));
 		
 		vbox2.setAlignment(Pos.CENTER);
+		
+		newGameButton.setOnAction(e -> {MainMenu.newGame(); });
+		newGameButton.setOnAction(e -> {MainMenu.loadGame(); });
+		newGameButton.setOnAction(e -> {MainMenu.helpMenu(); });
+		newGameButton.setOnAction(e -> {MainMenu.exitGame(); });
 	}
+	
+	public static void newGame()
+	{
+		
+	}
+	
+	public static void loadGame()
+	{
+		
+	}
+	
+	public static void helpMenu()
+	{
+		
+	}
+	
+	public static void exitGame()
+	{
+		System.exit(0);
+	}
+	
+	
 }
