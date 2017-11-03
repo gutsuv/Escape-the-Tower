6c4d543539dc75568b894b3da425c24d6eb89832
swilliams21 <swilliams21@ggc.edu>
2017-11-03 14:24:18 -0400

    I added a model to hold rooms and stuff

---
+++ src/Model.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Model {
+	/*
+	 * This class holds game stuff
+	 * when the game is loaded,
+	 * load rooms and other variables here
+	 * format I recommend is something along the lines of
+	 *<roomId/array[1],array[2],array[3]/other>
+	 *it's an example of how to do it
+	 * use the split command for "/" and ","
+	 * divide it into from 1 to 1/1/1 to 1/1,1,1,/1
+	 * 
+	 * loaded from file
+	 */
+	Map<Double,Room> roomsmap = new HashMap<>();
+	ArrayList<Item> inventory = new ArrayList<Item>();
+	public Model() 
+	{
+		//delete this code vvvv
+		Room r1 = new Room(1.0,false,"","");
+		roomsmap.put(r1.getRoomId(),r1);
+		//delete this code ^^^^
+	}
+	
+	
+}
