be86b9c4ace11fe14f067d1a858a49d4ce907e31
swilliams21 <swilliams21@ggc.edu>
2017-11-11 16:26:12 -0500

    from file contructor

--- src/Consumable.java
+++ src/Consumable.java
@@ -10,7 +10,24 @@ public class Consumable extends Item
 		this.healthEffect = 0;
 		this.maxHealthEffect = 0;
 	}
-
+	public Consumable(String loadedString) {
+		//loaded from file
+		//int itemID, String itemName, int healthEffect, int maxHealthEffect
+		//String itemDescShort, String itemDescLong
+		try {
+			String[] splitString = loadedString.split("|");
+			itemId = Integer.parseInt(splitString[0]);
+			itemName = splitString[1];
+			healthEffect = Integer.parseInt(splitString[2]);
+			maxHealthEffect = Integer.parseInt(splitString[3]);
+			itemDescShort = splitString[4];
+			itemDescLong = splitString[5];
+		}catch(Exception E)
+		{
+			System.out.println("Text Spencer Williams to fix this. I might have messed up a TextFile."
+					+ "I can fix it in like 30 minutes. Include Consumable in the text message");
+		}
+	}
 	public Consumable(int itemID, String itemName, String itemDesc, int healthEffect, int maxHealthEffect) 
 	{
 		super(itemID, itemName, itemDesc);
