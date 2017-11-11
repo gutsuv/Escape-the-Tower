d3d0e92278831ca1278342334a57e039788a85c9
swilliams21 <swilliams21@ggc.edu>
2017-11-11 15:06:48 -0500

    modified TextFile and added a contructor to Rooms that can receive a String input that is one line from the TextFile.

--- src/Room.java
+++ src/Room.java
@@ -1,19 +1,50 @@
+import java.lang.reflect.Array;
+
 public class Room
 {
-	protected double roomId;
+	protected int roomId;
 	protected String roomDescription;
-	String[] doors;
-	protected String doorDescription;
+	int[] exits;
+	char[] direction;
+	String[] exitDescription;
 
 	public Room() 
 	{
 	}
+	
+	public Room(String loadedString)
+	{
+		// loaded from file
+		//int roomId, String roomDescription, int exitRoomId, char direction, String doorDescription
+		try {
+			String[] splitString = loadedString.split("|");
+			roomId = Integer.parseInt(splitString[0]);
+			roomDescription = splitString[1];
+			//exits
+			String[] tempString1 = splitString[2].split("/");
+			String[] tempString2 = splitString[3].split("/");
+			int i = Array.getLength(tempString1);
+			exits = new int[i];
+			direction = new char[i];
+			while(i>0) {
+				i--;
+				exits[i] = Integer.parseInt(tempString1[i]);
+				direction[i] = tempString2[i].charAt(0);;
+			}
+			exitDescription = splitString[4].split("/");
+		}catch(Exception E)
+		{
+			System.out.println("Text Spencer Williams to fix this. I might have messed up a TextFile."
+					+ "I can fix it in like 30 minutes. Include RoomsA in the text message");
+		}	
+	}
 
-	public Room(double roomId, boolean isLocked, String roomDescription, String doorDescription)
+	public Room(int roomId, boolean isLocked, String roomDescription, String doorDescription)
 	{
 		this.roomId = roomId;
 		this.roomDescription = roomDescription;
-		this.doorDescription = doorDescription;
+		//this.exitDescription = doorDescription;
+		//multiple exits. Doesn't make sense to have door as one description.
 	}
 
 	public double getRoomId()
@@ -21,7 +52,7 @@ public class Room
 		return roomId;
 	}
 
-	public void setRoomId(double roomId)
+	public void setRoomId(int roomId)
 	{
 		this.roomId = roomId;
 	}
@@ -36,24 +67,24 @@ public class Room
 		this.roomDescription = roomDescription;
 	}
 
-	public String[] getDoors()
+	public int[] getDoors()
 	{
-		return doors;
+		return exits;
 	}
 
-	public void setDoors(String[] doors)
+	public void setDoors(int[] doors)
 	{
-		this.doors = doors;
+		this.exits = exits;
 	}
 
-	public String getDoorDescription()
+	public String[] getDoorDescription()
 	{
-		return doorDescription;
+		return exitDescription;
 	}
 
-	public void setDoorDescription(String doorDescription)
+	public void setDoorDescription(String[] doorDescription)
 	{
-		this.doorDescription = doorDescription;
+		this.exitDescription = doorDescription;
 	}
 	
 	public void enterRoom()
