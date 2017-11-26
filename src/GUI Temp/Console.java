9997987e06e12b59bc0e88db800c8b9f19c8eefb
Samuel Berende <sberende@ggc.edu>
2017-11-26 13:56:34 -0500

    Console Temp

---
+++ src/GUI Temp/Console.java
@@ -0,0 +1,37 @@
+package View;
+
+   
+
+   
+
+public class Console implements UserInterface{
+
+	   
+	
+	public void print(String string) {
+		System.out.print(string);
+	}
+
+	   
+	
+	public void println(String string) {
+		System.out.println(string);
+	}
+
+
+
+	@Override
+	public void updateUI(String printRoomMenu) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void updateUI(String printRoomMenu, int i) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
