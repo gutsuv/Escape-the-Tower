e2926cb9483647ae055ca25ea92cc96ac68344bb
Samuel Berende <sberende@ggc.edu>
2017-11-26 13:48:20 -0500

    Formating for comparision

--- src/GUI Temp/Controller.java
+++ src/GUI Temp/Controller.java
@@ -6,80 +6,97 @@ import Model.Artifact;
 import Model.Floor;
 import Model.Player;
 
-public class Controller {
+public class Controller
+{
 
 	ArrayList<Floor> floors;
-	int inRoom=1;
-	int onFloor=6;
-	
-	public int getOnFloor() {
+	int inRoom = 1;
+	int onFloor = 6;
+
+	public int getOnFloor()
+	{
 		return onFloor;
 	}
-	public void setOnFloor(int onFloor) {
+
+	public void setOnFloor(int onFloor)
+	{
 		this.onFloor = onFloor;
 	}
-	public int getInRoom() {
+
+	public int getInRoom()
+	{
 		return inRoom;
 	}
-	public void setInRoom(int inRoom) {
+
+	public void setInRoom(int inRoom)
+	{
 		this.inRoom = inRoom;
 	}
+
 	public Floor getFloor(int i)
 	{
-		for(Floor f:floors)
+		for (Floor f : floors)
 		{
-			if(f.getId()==i)
+			if (f.getId() == i)
 			{
 				return f;
 			}
 		}
 		return null;
 	}
-	Player player= new Player();
-	public String inventaryList() {
-		String toRet="";
-		for(Artifact a:player.inventary)
+
+	Player player = new Player();
+
+	public String inventaryList()
+	{
+		String toRet = "";
+		for (Artifact a : player.inventary)
 		{
-			toRet=toRet+"\n"+a.getName();
+			toRet = toRet + "\n" + a.getName();
 		}
-		if(toRet.isEmpty())
+		if (toRet.isEmpty())
 		{
 			return "There are no items in inventory";
 		}
-return toRet;
+		return toRet;
 	}
+
 	public boolean use(String item)
 	{
-		for(Artifact a:player.inventary)
+		for (Artifact a : player.inventary)
 		{
-			if(a.getName().equals(item))
+			if (a.getName().equals(item))
 			{
-				//Use ti
+				// Use ti
 				return true;
 			}
 		}
 		return false;
 	}
+
 	public boolean drop(String item)
 	{
-		for(Artifact a:player.inventary)
+		for (Artifact a : player.inventary)
 		{
-			if(a.getName().equals(item))
+			if (a.getName().equals(item))
 			{
-				
+
 				return true;
 			}
 		}
 		return false;
 	}
+
 	public boolean pickup(Artifact item)
 	{
 		player.inventary.add(item);
-		
+
 		return true;
 	}
-	private Controller() {
-		floors=new ArrayList<>();
+
+	private Controller()
+	{
+		floors = new ArrayList<>();
 		floors.add(new Floor(1));
 		floors.add(new Floor(2));
 		floors.add(new Floor(3));
@@ -93,24 +110,29 @@ return toRet;
 		player.setMonsterScore(0);
 		player.setGameScore(0);
 	}
+
 	public Player getPlayer()
 	{
-			return player;
+		return player;
 	}
+
 	public void setPlayerName(String name)
 	{
 		player.setPlayerName(name);
 	}
+
 	public void setPlayerGender(String gender)
 	{
 		player.setGender(gender);
 	}
+
 	private static Controller controller;
+
 	public static Controller eInstance()
 	{
-		if(controller==null)
+		if (controller == null)
 		{
-			controller= new Controller();
+			controller = new Controller();
 		}
 		return controller;
 	}
