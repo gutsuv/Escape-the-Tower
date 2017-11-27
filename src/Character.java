50d36116d03d25a220abb04c0ad95901fbd0a636
Samuel Berende <sberende@ggc.edu>
2017-11-26 20:18:31 -0500

    Character Comments

--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,14 @@
+/**
+ * Class: 
+ * @author Team Hydra
+ * @version 1.0
+ * Course: ITEC 3860, Fall 2017
+ * Written: 
+ * 
+ * This class – now describe what the class does 
+ * 
+ * Purpose: – Describe the purpose of this class
+ */
 
 public abstract class Character
 {
@@ -9,6 +20,12 @@ public abstract class Character
 
 	public abstract int dealDamage();
 
+	/**
+     * Method: receiveDamage()
+     * [What the method does]
+     * @param damage int value representing...
+     * @return nothing
+     */
 	public void receiveDamage(int damage)
 	{
 		health = health - damage;
@@ -24,31 +41,67 @@ public abstract class Character
 		}
 	}
 
+	/**
+     * Method: getHealth()
+     * [What the method does]
+     * @param none
+     * @return health int value representing...
+     */
 	public int getHealth()
 	{
 		return health;
 	}
 
+	/**
+     * Method: getMaxHealth()
+     * [What the method does]
+     * @param none
+     * @return maxHealth int value representing...
+     */
 	public int getMaxHealth()
 	{
 		return maxHealth;
 	}
 
+	/**
+     * Method: getDefense()
+     * [What the method does]
+     * @param none
+     * @return defense int value representing...
+     */
 	public int getDefense()
 	{
 		return defense;
 	}
 
+	/**
+     * Method: isAlive()
+     * [What the method does]
+     * @param none
+     * @return defense boolean value representing...
+     */
 	public boolean isAlive()
 	{
 		return alive;
 	}
 
+	/**
+     * Method: setAlive()
+     * [What the method does]
+     * @param alive boolean value representing...
+     * @return nothing
+     */
 	public void setAlive(boolean alive)
 	{
 		this.alive = alive;
 	}
 
+	/**
+     * Method: toString()
+     * [What the method does]
+     * @param none
+     * @return nothing
+     */
 	@Override
 	public String toString()
 	{
